d152b664e5eeaa1eb1eea20492b9466e5af34b7c
unknown <AJ98150@logti-a3322-04.ens.ad.etsmtl.ca>
2015-08-08 14:20:11 -0400

    base files

---
+++ cfg-contactform-30/inc/contactform.config.php
@@ -0,0 +1,19 @@
+<?php
+$cfg['email_address'] = 'info@ventdecheznous.com';
+$cfg['email_from'] = '';
+$cfg['email_address_cc'] = '';
+$cfg['email_address_bcc'] = '';
+$cfg['timezone'] = 'America/Denver';
+$cfg['form_name'] = 'Vent de chez nous';
+$cfg['form_validationmessage'] = '';
+$cfg['form_errormessage_captcha'] = '';
+$cfg['form_errormessage_emptyfield'] = '';
+$cfg['form_errormessage_invalidemailaddress'] = 'Mauvais courriel';
+$cfg['form_redirecturl'] = 'http://www.ventdecheznous.com';
+$cfg['adminnotification_subject'] = 'Nouveau message';
+$cfg['usernotification_inputid'] = '';
+$cfg['usernotification_insertformdata'] = '';
+$cfg['usernotification_format'] = '';
+$cfg['usernotification_subject'] = '';
+$cfg['usernotification_message'] = '';
+?>
